4762601f2f8f1a2a33af4b2262c76df890d5f247
Shrikar <Shrikar@DESKTOP-RJLKK58>
2019-12-12 15:07:09 +0530

    First commit

---
+++ advanced-jaxrs/src/main/java/org/rest/MyResources.java
@@ -0,0 +1,16 @@
+package org.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("test")
+public class MyResources {
+ 
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMetedata() {
+		return "It works";	
+	}
+}
